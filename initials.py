266e8f5970e61845ed2ec6f4835cf396bac0078b
MENSAHDANIEL06 <208027812+MENSAHDANIEL06@users.noreply.github.com>
2026-06-01 11:31:11 +0000

    Assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+# Name: Daniel Mensah
+# STUDENT ID: 2425404421
+# Email: dmenah5021@gmail.com
+# Grader: Carolyn
+#
+# On my honor, Daniel Mensah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Print the small initials and large stylized initials for DMD.
+    print()
+    print("...DMD")
+    print()
+    print("...DDDDDDDDDD........MM........MM.....DDDDDDDDDD....")
+    print("...DD........DD......MMM......MMM.....DD........DD..")
+    print("...DD........DD......MMMM....MMMM.....DD........DD..")
+    print("...DD........DD......MM.MM..MM.MM.....DD........DD..")
+    print("...DD........DD......MM..MMMM..MM.....DD........DD..")
+    print("...DD........DD......MM...MM...MM.....DD........DD..")
+    print("...DD........DD......MM........MM.....DD........DD..")
+    print("...DD........DD......MM........MM.....DD........DD..")
+    print("...DD........DD....MM........MM...DD........DD..")
+    print("...DDDDDDDDDD......MM........MM...DDDDDDDDDD....")
+    print()
 
 
 main()
